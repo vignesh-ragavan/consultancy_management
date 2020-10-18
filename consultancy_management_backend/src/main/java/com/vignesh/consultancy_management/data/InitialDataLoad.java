76e6489ac4239b476d5eb0d53990377dc3558e62
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-10-18 01:21:27 -0700

    load intial data

---
+++ consultancy_management_backend/src/main/java/com/vignesh/consultancy_management/data/InitialDataLoad.java
@@ -0,0 +1,22 @@
+package com.vignesh.consultancy_management.data;
+
+import com.vignesh.consultancy_management.domain.Capability;
+import com.vignesh.consultancy_management.repositories.CapabilityRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InitialDataLoad
+{
+
+    @Bean
+    CommandLineRunner LoadDB (CapabilityRepository capabilityRepository){
+        return args -> { //lambda expression
+            capabilityRepository.save(new Capability("Java", 100, 50));
+            capabilityRepository.save(new Capability("ReactJS",70,20));
+            capabilityRepository.save(new Capability("Python", 200, 100));
+        };
+    }
+}
+
